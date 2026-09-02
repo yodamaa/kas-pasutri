91f067b2c7fd25937430638dca058cbd33c2440f
rehan <rehanej47@gmail.com>
2026-09-02 09:19:01 +0700

    fix: tambah terjemahan ID avatar picker (apply button & upload prompt)

---
+++ lang/vendor/filament-avatar-picker/id/avatar.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'title' => 'Perbarui Foto Profil',
+    'description' => 'Pilih avatar dari koleksi kami atau unggah gambar sendiri.',
+    'gallery_tab' => 'Galeri',
+    'upload_tab' => 'Unggah',
+    'apply_button' => 'Terapkan',
+    'no_avatars' => 'Tidak ada avatar di galeri.',
+    'upload_prompt' => 'Pilih avatar atau <span class="filament-avatar-picker-browse-link">Jelajahi</span>',
+];
